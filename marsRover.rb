4b393942cd46c15ea5d275c74143c8e1b3152b6f
EDWIN AU <edwinau@rogers.com>
2017-04-15 16:16:08 -0400

    initializing 1st Rover

--- marsRover.rb
+++ marsRover.rb
@@ -1,34 +1,24 @@
 class Mars
   attr_accessor :x, :y, :direction
+
     def initialize(x,y,d)
       @x = x
       @y = y
       @direction = direction
-
-    def turn do |x|
-        case x = "L" || "R" do
-        when "L"
-          @y-1
-        when "R"
-          @y+1
-        end
     end
 
-    def turn
-      N=0
-      E=1
-      S=2
-      W=3
+
+    def read_instruction
+
     end
 
     def move
 
     end
 
-    def read_instruction
+    def turn
     end
+
 end
 
 mars = Mars.new (1,2,N)
-
-puts mars.input(LMLMLMLMLMLLM)
